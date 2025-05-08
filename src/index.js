9ac124039308eb7a50e0dc10bc9bf325b1904ba9
rileylorgelly <163364897+rileylorgelly@users.noreply.github.com>
2025-05-08 20:42:51 +1200

    added bird grid image generation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 document.querySelectorAll(".bird-grid-item").forEach((item) => {
     const bird = item.dataset.birdtype;
-    item.innerHTML = `<div class="bird-grid-bottom-bar"><h2></h2><img id="bird-readmore" src="src/img/misc/readmore.svg" alt="bird-readmore"></div>`
+    item.innerHTML = `<div class="bird-grid-bottom-bar"><h2></h2><img class="bird-readmore" data-birdtype="${bird}" src="src/img/misc/readmore.svg" alt="bird-readmore"></div>` 
+});
+
+document.querySelectorAll(".bird-grid-item").forEach((item) => {
+    const bird = item.dataset.birdtype;
+    item.style.backgroundImage = `url('src/img/birds/${bird}.png')`
 });
